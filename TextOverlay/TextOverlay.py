ae58be9d70d4974d58ab1b33100ac4c6b3204600
JingtongHu <j278hu@uwaterloo.ca>
2022-01-15 18:30:37 -0500

    add render text support

--- TextOverlay/TextOverlay.py
+++ TextOverlay/TextOverlay.py
@@ -14,17 +14,18 @@ def add_text_to_img(texts):
     width, height = image.size
 
     # resize canvas
-    nof_canvas = len(texts)
+    nof_canvas = len(texts) - 1
     x_gap = 200  # x gap between canvas
     canvas_width = int((width - x_gap) / nof_canvas - x_gap)
+    half_canvas = int(canvas_width / 2)
 
     y_ratio = 0.3  # y percentage of the table area
     y_gap = 200  # y gap between canvas
     canvas_height = int(height * y_ratio - y_gap * 2)
 
     color = (255, 255, 255)
-    font_size = 150
-    font = ImageFont.truetype('assets/PersonnBold-x3l4R.ttf', font_size)
+    font_size = 90
+    font = ImageFont.truetype('assets/Quicksand-Medium.ttf', font_size)
 
     corner_width = 50
     corner_raius = 200
@@ -34,20 +35,38 @@ def add_text_to_img(texts):
         y_canvas = int(height * (1 - y_ratio) + y_gap)
 
         x_text_gap = 150
-        y_text_gap = 150
-        y_text = 150
+        y_text_gap = 130
+        y_text_height = 120
         image = Image.open(src_dir + img_name + str(idx) + postfix)
-        for j in range(0, nof_canvas, 1):
+        text_pos_y = 0
+        for j in range(0, len(texts), 1):
             image_draw = ImageDraw.Draw(image)
-            image_draw.rounded_rectangle((x_canvas, y_canvas, x_canvas + canvas_width, y_canvas + canvas_height),
-                            fill="black", outline="grey", width=corner_width, radius=corner_raius)
-            text_pos_x = x_canvas + x_text_gap
-            text_pos_y = y_canvas + y_text_gap
-            for text in texts[j]:
-                image_draw.text((text_pos_x, text_pos_y), text, font=font, fill=color)
-                text_pos_y += y_text
+            if j != 1:
+                image_draw.rounded_rectangle((x_canvas, y_canvas, x_canvas + canvas_width, y_canvas + canvas_height),
+                                             fill="black", outline="grey", width=corner_width, radius=corner_raius)
 
-            x_canvas += canvas_width + x_gap
+            if j == 0 or j == 2:
+                text_pos_y = y_canvas + y_text_gap
+                for text in texts[j]:
+                    text_pos_x = x_canvas + x_text_gap
+                    image_draw.text((text_pos_x, text_pos_y), text[0][0], font=font, fill=text[1])
+                    text_pos_y += y_text_height
+                text_pos_y = y_canvas + y_text_gap
+                for text in texts[j]:
+                    text_pos_x = x_canvas + half_canvas + x_text_gap
+                    image_draw.text((text_pos_x, text_pos_y), text[0][1], font=font, fill=text[1])
+                    text_pos_y += y_text_height
+            else:
+                if j != 1:
+                    text_pos_y = y_canvas + y_text_gap
+                for k in range(len(texts[j])):
+                    text = texts[j][k]
+                    text_pos_x = x_canvas + (half_canvas if k % 2 else 0) + x_text_gap
+                    image_draw.text((text_pos_x, text_pos_y), text[0], font=font, fill=text[1])
+                    if k % 2:
+                        text_pos_y += y_text_height
+
+            if j != 0:
+                x_canvas += canvas_width + x_gap
         image.save(dest_dir + img_name + str(idx) + postfix)
         image.close()
-    canvas.close()
